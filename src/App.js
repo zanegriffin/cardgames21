bb2c1f874d9125ad94d4caabeb61bbe3136856bb
Zane <zanebgriffin@gmail.com>
2021-02-24 11:21:11 -0600

    added api calls

--- src/App.js
+++ src/App.js
@@ -4,18 +4,46 @@ import './App.css';
 function App() {
 
   const [deckID, setDeckID] = useState('')
+  const [hand, setHand] = useState([])
+  const [remainingCards, setRemainingCards] = useState(0)
 
   const getDeck = () => {
     fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=6')
     .then(response => {
-
+      setDeckID(response.deck_id)
+      setRemainingCards(response.remaining)
     })
+  }
+
+  const drawCards = () => {
+    fetch(`https://deckofcardsapi.com/api/deck/${deckID}/draw/?count=2`)
+    .then(response => {
+      setHand(response.cards)
+      setRemainingCards(response.remaining)
+    })
+  }
+
+  const hitCard = () => {
+    fetch(`https://deckofcardsapi.com/api/deck/${deckID}/draw/?count=1`)
+    .then(response => {
+      setHand([...hand, response.cards])
+    })
+  }
+
+  const newRound = () => {
+    setHand([])
+  }
+
+  const shuffleDeck = () => {
+    fetch(`https://deckofcardsapi.com/api/deck/${deckID}/shuffle/`)
   }
 
 
   return (
     <div className="App">
-      
+      <div className='table'>
+        
+      </div>
     </div>
   );
 }
