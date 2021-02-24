7086ef4c901b5a56638e7dba5014fe40dea20557
Zane <zanebgriffin@gmail.com>
2021-02-24 11:03:12 -0600

    fetching full deck and setting up id

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react'
 import './App.css';
 
 function App() {
+
+  const [deckID, setDeckID] = useState('')
+
+  const getDeck = () => {
+    fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=6')
+    .then(response => {
+
+    })
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
